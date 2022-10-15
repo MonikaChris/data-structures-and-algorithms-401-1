5e25b55066e623ca3ea329e3dc31305df5635b01
Monika <monidavies@gmail.com>
2022-10-15 15:29:00 -0700

    Adds str method

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -10,14 +10,22 @@ class LinkedList:
     def insert(self, value):
         node = Node(value)
         if self.head is not None:
-            node.next = self.head
+            node.next_ = self.head
         self.head = node
 
+    def __str__(self):
+        current = self.head
+        printout = ''
+        while current is not None:
+            printout += f'{{ {current.value} }} -> '
+            current = current.next_
+        printout += f'NULL'
+        return printout
 
 class Node:
-    def __init__(self, value, _next=None):
+    def __init__(self, value, next_=None):
         self.value = value
-        self._next = _next
+        self.next_ = next_
 
 class TargetError:
     pass
