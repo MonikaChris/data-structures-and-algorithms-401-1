44a64b88019ff0ce7b8dbe6918d6836d1343e1e2
Monika <monidavies@gmail.com>
2022-10-15 15:04:13 -0700

    Creates Node and LinkedList classes

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,16 +1,21 @@
 class LinkedList:
     """
-    Put docstring here
+    Pass in a head node to implement a linked list.
     """
 
-    def __init__(self):
+    def __init__(self, head=None):
         # initialization here
-        pass
+        self.head = head
 
     def some_method(self):
         # method body here
         pass
 
+
+class Node:
+    def __init__(self, value, _next=None):
+        self.value = value
+        self._next = _next
 
 class TargetError:
     pass
