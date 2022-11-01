08218fec26f8c9515dd3266c4525919ef3dd56db
Monika <monidavies@gmail.com>
2022-10-31 22:06:54 -0700

    Adds stack and methods

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,11 +1,17 @@
+class Node:
+
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class Queue:
     """
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, front=None):
+        self.front = front
 
     def some_method(self):
         # method body here
