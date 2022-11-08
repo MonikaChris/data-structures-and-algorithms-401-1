681e458294600c71197db47bc725c62a33528c56
Monika <monidavies@gmail.com>
2022-11-07 20:14:37 -0800

    Adds pseudo-queue

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,24 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        self.main = Stack()
+        self.temp = Stack()
+
+    def enqueue(self, value):
+        self.main.push(value)
+
+    def dequeue(self):
+        if self.main.is_empty():
+            return None
+
+        while not self.main.is_empty():
+            self.temp.push(self.main.pop())
+
+        last_value = self.temp.pop()
+
+        while not self.temp.is_empty():
+            self.main.push(self.temp.pop())
+
+        return last_value
+
