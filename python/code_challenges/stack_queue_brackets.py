9d3eef6b9873f78bdb0b7b08b997b1392b954c00
Monika <monidavies@gmail.com>
2022-11-14 19:51:50 -0800

    Adds bracket_validation

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,36 @@
-from data_structures.queue import Queue
+from data_structures.stack import Stack
 
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(str):
+    balanced = True
+    opened = Stack()
+    brackets = {'{': 'open',
+                '(': 'open',
+                '[': 'open',
+                '}': 'close',
+                ')': 'close',
+                ']': 'close'
+                }
+
+    match = {'{': '}', '(': ')', '[': ']'}
+
+    for char in str:
+        if char in brackets.keys():
+            if brackets[char] == 'open':
+                opened.push(char)
+            else:
+                if opened.is_empty():
+                    balanced = False
+                    return balanced
+                if char == match[opened.peek()]:
+                    opened.pop()
+                else:
+                    balanced = False
+                    return balanced
+
+    if not opened.is_empty():
+        balanced = False
+
+    return balanced
+
+
