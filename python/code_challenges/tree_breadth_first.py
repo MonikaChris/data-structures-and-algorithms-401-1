599be3edfd9c4d2b361ef0c3695737182ba6cfdc
Monika <monidavies@gmail.com>
2022-11-28 19:35:50 -0800

    Adds breadth-first tree search

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,26 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.queue import Queue
 
 
-def breadth_first():
-    pass
+def breadth_first(tree):
+    nodes = Queue()
+    values = []
+
+    if tree.root:
+        nodes.enqueue(tree.root)
+    else:
+        return values
+
+    while not nodes.is_empty():
+        current = nodes.dequeue()
+
+        if current:
+            values.append(current.value)
+
+        if current.left:
+            nodes.enqueue(current.left)
+
+        if current.right:
+            nodes.enqueue(current.right)
+
+    return values
